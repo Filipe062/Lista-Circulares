9e005dcf427f517e88d5559afb5669f547d6c039
Luiz Filipe Ferreira Gonçalves <luisfhylipe@gmail.com>
2025-12-14 12:40:40 -0300

    Primeiro commit

---
+++ listacirculares/src/main/java/dio/me/Main.java
@@ -0,0 +1,23 @@
+package dio.me;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaCircula<String> minhaListaCircular = new ListaCircula<>();
+
+        minhaListaCircular.add("c0");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0);
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c1");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c2");
+        minhaListaCircular.add("c3");
+        System.out.println(minhaListaCircular);
+
+        System.out.println(minhaListaCircular.get(0));
+        System.out.println(minhaListaCircular.get(1));
+    }
+}
